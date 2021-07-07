8f020b929797edd9deb0a720237ab5ffc6b0c25e
PJHgh <whdgjs2323@naver.com>
2021-07-08 00:51:02 +0900

    update
    update

--- naver webtoon cote/Q2.py
+++
@@ -1,7 +0,0 @@
-def solution(word):
-    w2v = {k:v for v,k in enumerate(list('AEIOU'))}
-    i2v = {'0':781, '1':156, '2':31, '3':6, '4':1}
-    answer = 0
-    for i, w in enumerate(word):
-        answer += i2v[str(i)]*w2v[w]+1
-    return answer

16f95601f46d62c978399fe9df67a9a2f6fd5801
PJHgh <whdgjs2323@naver.com>
2021-07-08 22:58:25 +0900

    20210708
    20210708

--- line_test/1_bisect.py
+++ line_test/1_bisect.py
@@ -5,41 +5,21 @@ list의 일부만 정렬해도, list 전체가 정렬될 수 있음.
 list 전체가 정렬되도록 하는 범위를 index 값으로 반환할 것.
 '''
 
-import bisect
+import heapq
 
 def solution(param0: list) -> tuple:
-    # 순서를 유지하며, index와 값을 저장하는 list 초기화
-    num_lst = []
-    idx_lst = []
-    # 가장 큰 값을 저장하는 변수 초기화
-    past_max_num = float('-inf')
-    # target 변수 초기화
-    start_position, end_position = len(param0), len(param0)
+    answer = []
+    param1 = param0.copy()
+    heapq.heapify(param1)
+    for i, p0 in enumerate(param0):
+        p1 = heapq.heappop(param1)
+        if p0 != p1:
+            answer.append(i)
+    return answer[0], answer[-1]
 
-    # 주어진 list 순회
-    for idx, num in enumerate(param0):
-        # 과거 값보다 크거나 같은 경우, list에 append 및 최대값 갱신
-        if past_max_num <= num:
-            num_lst.append(num)
-            idx_lst.append(idx)
-            past_max_num = num
-        # 과거 값보다 작은 경우, 정렬해야 하는 범위를 탐색
-        else:
-            # 현재 위치를 포함하여 범위 지정
-            end_position = idx
-            # 저장된 list는 정렬되어 있으므로, 이진탐색을 통해 위치 파악
-            lst_in_idx = bisect.bisect_right(num_lst, num)
-            # 예외처리를 포함하여, start_position 저장
-            start_position = min(start_position, idx_lst[lst_in_idx] if len(idx_lst) != lst_in_idx else end_position)
-            # 정렬된 범위만 남도록 list를 갱신
-            num_lst = num_lst[:lst_in_idx]
-            idx_lst = idx_lst[:lst_in_idx]
-
-    return start_position, end_position
-
-# print(solution([3, 2, 4, 2, 15, 12, 9, 16, 18, 19]))
-# print(solution([1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]))
-# print(solution([0,3,2,1,4,5,6,3,4,10,11,12]))
-# print(solution([0,1,1,1,1,2,1,3,1,9]))
-# print(solution([0]))
-# print(solution([1,3,4,2,5,6]))
+print(solution([3,2,4,2,15,12,9,16,18,19]))
+print(solution([1,2,4,7,10,11,7,12,6,7,16,18,19]))
+print(solution([0,3,2,1,4,5,6,3,4,10,11,12]))
+print(solution([0,1,1,1,1,2,1,3,1,9]))
+print(solution([1,0]))
+print(solution([1,3,4,2,5,6]))
--- line_test/2.py
+++ line_test/2.py
@@ -9,68 +9,31 @@ ex2) 2 = 2^1
 import sys
 import math
 
-def get_prime(n: int) -> list:
-    ''' n까지의 소수를 구하는 에라스토테네스의 체 함수 '''
-    # 각 index가 수를 표현하도록 list 초기화
-    lst = [False]*2 + [True]*(n-1)
-
-    # n의 제곱근을 포함하는 범위까지만 순회
-    for i in range(math.ceil(math.sqrt(n))) :
-        # list의 값이 True인 경우만 동작
-        if lst[i] :
+def get_prime(n):
+    lst = [True for i in range(n+1)]
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if lst[i] == True:
             j = 2
-            # 해당 index의 배수인 모든 index는 False로 변환
-            while i*j < n :
-                lst[i*j] = False
+            while i * j <= n:
+                lst[i * j] = False
                 j += 1
-    
-    # index가 True인 모든 수를 list 형태로 반환
-    return [i for i in range(len(lst)) if lst[i]==True]
-
-def factorization(primes: list, n: int) -> list:
-    ''' n의 소인수 분해 식을 만드는 함수 '''
-    lst = []
-    # n이하의 모든 소수를 순회
-    for prime in primes:
-        cnt = 0
-        tmp = n
-        # 해당 소수로 n을 몇 번 나눌 수 있는지 저장
-        while True:
-            q, r = divmod(tmp, prime)
-            if r == 0:
-                cnt += 1
-                tmp = q
-            else:
-                break
-        # 한 번 이상 나눠진 경우 list에 append
-        if cnt > 0:
-            lst.append((prime, cnt))
-    return lst
-
-def main(line: str) -> str:
-    ''' 위 함수를 호출하는 main 함수 '''
-    # 위 함수를 호출
-    n = int(line)
-    primes = get_prime(n)
-    factorization_lst = factorization(primes, n)
-
-    # 필요한 변수 초기화
-    factorization_expression = []
-    divisor_cnt = 1
-
-    # 소인수 분해 식을 저장하고 있는 list를 순회
-    for prime, cnt in factorization_lst:
-        string = str(prime) + '^' + str(cnt)
-        factorization_expression.append(string)
-        divisor_cnt *= (cnt+1)
-
-    # 약수의 갯수 및 소인수 분해 표현을 string 형태로 변환
-    divisor_cnt = str(divisor_cnt)
-    if len(factorization_expression) == 1:
-        factorization_expression = factorization_expression[0]
-    else:
-        factorization_expression = ' * '.join(factorization_expression)
-    return factorization_expression + '\n' + divisor_cnt
+    return [i for i in range(2, n+1) if lst[i]]
 
 line = sys.stdin.readline()
-print(main(line))
+
+n = int(line)
+primes = get_prime(n)
+
+num = 1
+form = []
+for p in primes:
+    d = 0
+    while n%p == 0:
+        d += 1
+        n /= p
+    if d > 0:
+        num *= d+1
+        form.append(str(p)+'^'+str(d))
+
+print(num)
+print(' * '.join(form))
